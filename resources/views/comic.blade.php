2b3693f309f0ef35057c0819e70925dc1fef4902
Ivanpepp <nastroivan3@gmail.com>
2021-10-26 16:48:54 +0200

    half adv section

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -18,8 +18,31 @@
                             <span class="upper-sticker sticker" >comic book</span>
                             <span class="lower-sticker sticker">view galley</span>
                         </div>
-                        
                     </div>
+                </div>
+                <div class="my-view2">
+                    <section id="description-section">
+                        <p class="adv">advertisement</p>
+                        <div class="description-wrapper">
+                            <div class="description">
+                                    <h1>Action comics #1000 ecc ecc ecc</h1>
+                                    <div class="price-wrapper">
+                                        <div class="price">
+                                            <span>U.S. Price:</span>
+                                            <span> soldi dinamici</span>
+                                            <span class="right">aviable</span>
+                                        </div>
+                                        
+                                    </div>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, magni aliquam officia odit nemo itaque nulla sit magnam, maiores nobis distinctio laborum? Veritatis porro cum, quae quia commodi ipsam deleniti!</p>
+                            </div>
+                            
+                            <img src="{{asset('images/pro-va.jpg')}}" alt="adv logo">
+                        
+                        </div>
+                    </section>
+                    
+
                 </div>
                 
             </div>
